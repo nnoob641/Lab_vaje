d07d0be4761fe6caf333be27e88d10f70a66326f
nnoob641 <41363777+nnoob641@users.noreply.github.com>
2018-10-19 10:36:44 +0200

    Changes on 19.10.2018

--- Lab1.R
+++ Lab1.R
@@ -43,4 +43,40 @@ matr <- matrix(c(1:12),nrow=4,ncol=3,T)
 colnames(matr) <- c("x","y","z")
 row.names(matr) <- c("a","b","c","d")
 
-#
+#-------------factors.txt------------
+#1)
+factor(c(1, 2, 3, 3, 5, 3, 2, 4, NA))
+# [1] 1    2    3    3    5    3    2    4    <NA>
+# Levels: 1 2 3 4 5
+
+#2)
+x <- c(11, 22, 47, 47, 11, 47, 11)
+factor(x, levels=c(11, 22, 47), ordered=TRUE)
+#[1] 11 22 47 47 11 47 11
+#Levels: 11 < 22 < 47
+
+
+#3)
+z <- c("p", "a" , "g", "t", "b")
+z[3] <- "b"
+#[1] "p" "a" "b" "t" "b"
+
+#4)
+u <- factor(c("p", "q", "p", "r", "q"))
+levels(u)[levels(u)=="p"] <- "w"
+
+
+#5)
+cs1 <- as.character(s1)
+cs2 <- as.character(s2)
+cc <- factor(c(cs1,cs2))
+
+#6)
+num <- iris$Sepal.Length
+cut(num,5)
+
+#7)
+x <- data.frame(q=c(2, 4, 6), p=c("a", "b", "c"))
+x[2] <- c("fertiliser1", "fertiliser2", "fertiliser3")
+
+
